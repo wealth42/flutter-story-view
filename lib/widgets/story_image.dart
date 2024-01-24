96c91d40694041be69f4e4014b33dd40421810b8
merahul31 <merahul31r@gmail.com>
2024-01-25 01:28:52 +0530

    flutter upgrade to 3.16.8 (doing testing)

--- lib/widgets/story_image.dart
+++ lib/widgets/story_image.dart
@@ -2,11 +2,11 @@ import 'dart:async';
 import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
-import '../utils.dart';
 import '../controller/story_controller.dart';
+import '../utils.dart';
 
 /// Utitlity to load image (gif, png, jpg, etc) media just once. Resource is
 /// cached to disk with default configurations of [DefaultCacheManager].
@@ -33,7 +33,7 @@ class ImageLoader {
         headers: this.requestHeaders as Map<String, String>?);
 
     fileStream.listen(
-      (fileResponse) {
+      (fileResponse) async {
         if (!(fileResponse is FileInfo)) return;
         // the reason for this is that, when the cache manager fetches
         // the image again from network, the provided `onComplete` should
@@ -46,8 +46,10 @@ class ImageLoader {
 
         this.state = LoadState.success;
 
-        PaintingBinding.instance!.instantiateImageCodec(imageBytes).then(
-            (codec) {
+        PaintingBinding.instance
+            .instantiateImageCodecWithSize(
+                await ImmutableBuffer.fromUint8List(imageBytes))
+            .then((codec) {
           this.frames = codec;
           onComplete();
         }, onError: (error) {
